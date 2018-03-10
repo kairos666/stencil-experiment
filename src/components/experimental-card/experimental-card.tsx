84e0db3a0899241c471d2fcf079b383c98dc24be
Unknown <musashi.miyamoto81@gmail.com>
2018-03-10 10:17:17 +0100

    [cmpnt] mutation observer for SLOT changes

--- src/components/experimental-card/experimental-card.tsx
+++ src/components/experimental-card/experimental-card.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State, Element } from '@stencil/core';
 
 @Component({
   tag: 'experimental-card',
@@ -9,13 +9,42 @@ export class ExperimentalCard {
   @Prop() supportingText: string;
   @Prop() illustration: string;
 
-  titleStyle = () => {
+  @State() hasActions: boolean;
+
+  @Element() cardEl: HTMLElement;
+  private actionsEl: HTMLElement;
+
+  private titleStyle = () => {
     return {
       backgroundImage: `url(${this.illustration})`,
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'bottom right 15%'
     }
   };
+
+  private actionsStyle = () => {
+    return {
+      display: (this.hasActions) ? '' : 'none'
+    }
+  };
+
+  componentDidLoad() {
+    this.actionsEl = this.cardEl.querySelector('.mdl-card__actions');
+    this.hasActions = (this.actionsEl.childElementCount !== 0);
+    this.slotMutationObserver();
+  }
+
+  
+  private slotMutationObserver(): void {
+    // mutation CB handler
+    let reactionCB = () => {
+      this.hasActions = (this.actionsEl.childElementCount !== 0);
+    };
+
+    // setting up mutation observer
+    let observer = new MutationObserver(reactionCB);
+    observer.observe(this.actionsEl, { childList: true });
+  }
 
   render() {
     return (
@@ -24,6 +53,9 @@ export class ExperimentalCard {
           <h2 class="mdl-card__title-text">{this.title}</h2>
         </div>
         <figcaption class="mdl-card__supporting-text">{this.supportingText}</figcaption>
+        <div class="mdl-card__actions mdl-card--border" style={this.actionsStyle()}>
+          <slot />
+        </div>
       </figure>
     );
   }
