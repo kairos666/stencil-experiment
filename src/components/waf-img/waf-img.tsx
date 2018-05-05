d8191d91a2467f21ea324dc0fdae9a0665ade268
David Maggi <david.maggi@orange.com>
2018-05-05 12:43:23 +0200

    [waf-img] basic img async placeholder
    - async loading of image
    - need to style when loading or broken
    - do the same with srcset
    - animate on change

---
+++ src/components/waf-img/waf-img.tsx
@@ -0,0 +1,72 @@
+import { Component, Prop, State, Watch } from '@stencil/core';
+
+@Component({
+  tag: 'waf-img',
+  styleUrl: 'waf-img.scss'
+})
+export class WafImg {
+    @Prop() src:string;
+    @Prop() srcset:string;
+    @Prop() alt:string;
+    @Prop() decoding:'sync'|'async'|'auto';
+    @Prop() width:string;
+    @Prop() height:string;
+    @Prop() sizes:string;
+    @State() innerSrc:string;
+    @State() isBroken:boolean = false;
+
+    render() {
+        const renderedEl = (this.innerSrc) ? (
+            <img src={this.innerSrc} alt={this.alt} width={this.width} height={this.height} sizes={this.sizes} srcset={this.srcset} decoding={this.decoding} />
+        ) : (this.isBroken) ? (
+            <span class="waf-img__broken" style={this.infoDynamicStyles()}>i'm broken</span>
+        ) : (
+            <span class="waf-img__loading" style={this.infoDynamicStyles()}>i'm loading</span>
+        )
+        return renderedEl;
+    }
+
+    componentDidLoad() {
+        // checks for at least "src" ("alt" is not necessary if only used for decoration)
+        if (!this.src) {
+            console.warn('waf-img | a src is required');
+            this.isBroken = true;
+        } else {
+            // async load when browser is available
+            const docState = document.readyState.toString();
+            if (docState === 'loaded' || docState === 'interactive' || docState === 'complete') {
+                // page is fully loaded
+                this.srcSwapHandler(this.src);
+            } else {
+                // wait for the page to be fully loaded
+                window.addEventListener('load', () => {
+                    this.srcSwapHandler(this.src);
+                });
+            }
+        }
+    }
+
+    infoDynamicStyles() {
+        return (this.width && this.height) ? {
+            display: 'inline-block',
+            width: `${this.width}px`,
+            height: `${this.height}px`
+        } : {}
+    }
+
+    @Watch('src')
+    srcSwapHandler(newValue:string) {
+        // first fetch the image (browser put it in cache)
+        fetch(this.src)
+            .then(() => {
+                // then add src set that will use that cache directly
+                this.isBroken = false;
+                this.innerSrc = newValue;
+            })
+            .catch(() => {
+                // if image can't be fetched
+                this.isBroken = true;
+                this.innerSrc = undefined;
+            });
+    }
+}
