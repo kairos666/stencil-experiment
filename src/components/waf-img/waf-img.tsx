fac0127f0aca01cb714bf21146c2a4acb83b1cca
David Maggi <david.maggi@orange.com>
2018-05-08 18:03:55 +0200

    [waf-img] add feature for loading only if both document is ready and image is visible

--- src/components/waf-img/waf-img.tsx
+++ src/components/waf-img/waf-img.tsx
@@ -1,10 +1,11 @@
-import { Component, Prop, State, Watch } from '@stencil/core';
+import { Component, Prop, State, Watch, Element } from '@stencil/core';
 
 @Component({
   tag: 'waf-img',
   styleUrl: 'waf-img.scss'
 })
 export class WafImg {
+    private intersectionObserver:IntersectionObserver;
     @Prop() src:string;
     @Prop() srcset:string;
     @Prop() alt:string;
@@ -12,8 +13,10 @@ export class WafImg {
     @Prop() width:string;
     @Prop() height:string;
     @Prop() sizes:string;
+    @Prop() visibleAsyncLoading:boolean = false;
     @State() innerSrc:string;
     @State() isBroken:boolean = false;
+    @Element() wafImg:Element;
 
     render() {
         const renderedEl = (this.innerSrc) ? (
@@ -37,8 +40,8 @@ export class WafImg {
         if (!this.src) {
             console.warn('waf-img | a src is required');
             this.isBroken = true;
-        } else {
-            // async load when browser is available
+        } else if(!this.visibleAsyncLoading) {
+            // async load when browser is available (not waiting for element to be visible in viewport)
             const docState = document.readyState.toString();
             if (docState === 'loaded' || docState === 'interactive' || docState === 'complete') {
                 // page is fully loaded
@@ -49,8 +52,36 @@ export class WafImg {
                     this.srcSwapHandler(this.src);
                 });
             }
+        } else {
+            // async load when browser is available (waiting for element to be visible in viewport)
+            // default is according to viewport and as soon as the first pixel is visible
+            this.intersectionObserver = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        // element is visible - but yet we make sure that the rest of the page is loaded
+                        const docState = document.readyState.toString();
+                        if (docState === 'loaded' || docState === 'interactive' || docState === 'complete') {
+                            // page is fully loaded
+                            this.srcSwapHandler(this.src);
+                        } else {
+                            // wait for the page to be fully loaded
+                            window.addEventListener('load', () => {
+                                this.srcSwapHandler(this.src);
+                            });
+                        }
+                        this.srcSwapHandler(this.src);
+                        observer.disconnect();
+                    }
+                });
+            });
+            this.intersectionObserver.observe(this.wafImg);
         }
     }
+
+    componentDidUnload() {
+        // cleanup observer if needed
+        if (this.intersectionObserver) this.intersectionObserver.disconnect();
+      }
 
     infoDynamicStyles() {
         return (this.width && this.height) ? {
