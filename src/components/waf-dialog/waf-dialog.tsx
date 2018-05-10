2e35a539a97d541e37ab33e47a0d548dd88f205a
David Maggi <david.maggi@orange.com>
2018-05-10 19:26:45 +0200

    [waf-dialog] focus trap behavior
    - npm package focus-trap
    - init focus trap

--- src/components/waf-dialog/waf-dialog.tsx
+++ src/components/waf-dialog/waf-dialog.tsx
@@ -1,4 +1,5 @@
 import { Component, Prop, State, Method, Element } from '@stencil/core';
+import focusTrapBuilder from 'focus-trap';
 
 /**
  * &lt;WAF-DIALOG&gt;
@@ -32,6 +33,7 @@ export class WafDialog {
     private closeAttrName:string = 'data-dialog-close';
     private uniqueId:number = Date.now();
     private backdropElt:Element;
+    private focusTrap;
     @Element() private wafDialogElt:Element;
     @State() isOpen:boolean = false;
     @Prop() preventBackdropClosing:boolean;
@@ -42,7 +44,7 @@ export class WafDialog {
         return [
             <div class="waf-dialog-backdrop" tabindex="-1" style={(!this.isOpen || this.noBackdrop) ? { display: 'none' } : { zIndex: String(this.backdropZIndex) }}></div>,
             <div aria-hidden={(!this.isOpen).toString()} aria-labelledby={this.idGenerator('title')} aria-describedby={this.idGenerator('description')} role="dialog"  style={(!this.isOpen) ? { display: 'none' } : { zIndex: String(this.dialogZIndex) }}>
-                <section role="document">
+                <section role="document" tabindex="-1">
                     <div id={this.idGenerator('description')} class="sr-only">Beginning of dialog window. Escape will cancel and close the window.</div>
                     <div id={this.idGenerator('title')}>
                         <slot name="title"/>
@@ -59,11 +61,23 @@ export class WafDialog {
         this.wafDialogElt.addEventListener('click', this.innerCloseHandler.bind(this), true);
 
         // listen to backdrop click
-        this.backdropElt = this.wafDialogElt.querySelector('.waf-dialog-backdrop')
+        this.backdropElt = this.wafDialogElt.querySelector('.waf-dialog-backdrop');
         this.backdropElt.addEventListener('click', this.backdropClickHandler.bind(this));
 
         // listen to escape key
         document.addEventListener('keydown', this.escapeKeyHandler.bind(this));
+
+        // setup focus trap
+        const fallback:HTMLElement = (this.wafDialogElt.querySelector('[role="document"]') as HTMLElement); // used if no focusable element was provided inside the dialog box
+        this.focusTrap = focusTrapBuilder((this.wafDialogElt as HTMLElement), {
+            onActivate: undefined,
+            onDeactivate: undefined,
+            initialFocus: undefined,
+            fallbackFocus: fallback,
+            escapeDeactivates: true,
+            clickOutsideDeactivates: false,
+            returnFocusOnDeactivate: true
+        });
     }
 
     componentDidUnload() {
@@ -99,11 +113,23 @@ export class WafDialog {
     private idGenerator(type:'title'|'description') { return `dialog-${this.uniqueId}-${type}` }
 
     @Method()
-    showModal() { this.isOpen = true } 
+    showModal() { 
+        this.isOpen = true;
+        this.focusTrap.activate();
+    } 
 
     @Method()
-    hideModal() { this.isOpen = false }
+    hideModal() { 
+        this.isOpen = false;
+        this.focusTrap.deactivate();
+    }
 
     @Method()
-    toggleModal() {  this.isOpen = !this.isOpen; }
+    toggleModal() {  
+        if (this.isOpen) {
+            this.hideModal();
+        } else {
+            this.showModal();
+        }
+    }
 }
