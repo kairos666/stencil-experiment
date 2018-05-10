494bc053c9f0d51cf8ac885feaa849ede9d25c9b
David Maggi <david.maggi@orange.com>
2018-05-10 18:27:50 +0200

    [waf-dialog] backdrop options and closing

--- src/components/waf-dialog/waf-dialog.tsx
+++ src/components/waf-dialog/waf-dialog.tsx
@@ -31,14 +31,16 @@ export class WafDialog {
     private dialogZIndex:number = 667;
     private closeAttrName:string = 'data-dialog-close';
     private uniqueId:number = Date.now();
+    private backdropElt:Element;
     @Element() private wafDialogElt:Element;
     @State() isOpen:boolean = false;
     @Prop() preventBackdropClosing:boolean;
     @Prop() noBackdrop:boolean;
+    @Prop() limitedHeight:boolean;
 
     render() {
         return [
-            <div class="waf-dialog-backdrop" tabindex="-1" style={(!this.isOpen) ? { display: 'none' } : { zIndex: String(this.backdropZIndex) }}></div>,
+            <div class="waf-dialog-backdrop" tabindex="-1" style={(!this.isOpen || this.noBackdrop) ? { display: 'none' } : { zIndex: String(this.backdropZIndex) }}></div>,
             <div aria-hidden={(!this.isOpen).toString()} aria-labelledby={this.idGenerator('title')} aria-describedby={this.idGenerator('description')} role="dialog"  style={(!this.isOpen) ? { display: 'none' } : { zIndex: String(this.dialogZIndex) }}>
                 <section role="document">
                     <div id={this.idGenerator('description')} class="sr-only">Beginning of dialog window. Escape will cancel and close the window.</div>
@@ -55,11 +57,16 @@ export class WafDialog {
     componentDidLoad() {
         // listen to click events passing through the dialog box (capture phase to avoid missing some)
         this.wafDialogElt.addEventListener('click', this.innerCloseHandler.bind(this), true);
+
+        // listen to backdrop click
+        this.backdropElt = this.wafDialogElt.querySelector('.waf-dialog-backdrop')
+        this.backdropElt.addEventListener('click', this.backdropClickHandler.bind(this));
     }
 
     componentDidUnload() {
-        // cleanup listener
+        // cleanup listeners
         this.wafDialogElt.removeEventListener('click', this.innerCloseHandler.bind(this), true);
+        this.backdropElt.removeEventListener('click', this.backdropClickHandler.bind(this));
     }
 
     private innerCloseHandler(evt:Event) {
@@ -75,6 +82,10 @@ export class WafDialog {
 
         // close dialog if the click event originated from an element with the close attribute
         if (found) this.hideModal();
+    }
+
+    private backdropClickHandler() {
+        if (!this.preventBackdropClosing && !this.noBackdrop) this.hideModal();
     }
 
     private idGenerator(type:'title'|'description') { return `dialog-${this.uniqueId}-${type}` }
