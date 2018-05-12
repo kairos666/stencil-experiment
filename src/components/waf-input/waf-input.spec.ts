047e383b0ecd7b0f486187818bcda0098b3cdf34
David Maggi <david.maggi@orange.com>
2018-05-12 16:56:33 +0200

    [TESTS] build tests for waf-input|waf-tabs|waf-dialog

---
+++ src/components/waf-input/waf-input.spec.ts
@@ -0,0 +1,8 @@
+import { WafInput } from './waf-input';
+
+describe('waf-input', () => {
+    it('should build', () => {
+        // test component class build
+        expect(new WafInput()).toBeTruthy();
+    });
+});
