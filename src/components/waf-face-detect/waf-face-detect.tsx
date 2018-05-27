8b24d5483ea601fa198bd14987af93fb9d9af683
David Maggi <david.maggi@orange.com>
2018-05-27 09:44:09 +0200

    [waf-face-detect] do face detection & output results
    - evt with detction data
    - draw circle on canvas

--- src/components/waf-face-detect/waf-face-detect.tsx
+++ src/components/waf-face-detect/waf-face-detect.tsx
@@ -1,4 +1,4 @@
-import { Component, Element } from '@stencil/core';
+import { Component, Element, Event, EventEmitter } from '@stencil/core';
 import { moduleInit } from '../../assets/wasmpico';
 
 declare const WebAssembly:any;
@@ -9,17 +9,19 @@ declare const WebAssembly:any;
 })
 export class WafFaceDetect {
     @Element() fdElt:HTMLElement;
+    @Event({eventName: 'waf.face-detector.detected'}) private wafFaceDetectorEE:EventEmitter;
     private fdCanvas:HTMLCanvasElement;
     private fdVideo:HTMLVideoElement;
     private fdWidth:number = 640;
     private fdHeight:number = 480;
+    private detectionThreshold = 3;
     private assetsPath:string = `${location.origin}/assets/`;
     private wasmFaceDetectorFile:string = 'wasmpico';
     private wasmFaceDetectorModule;
 
     render() {
         return [
-            <video autoplay playsinline width={this.fdWidth} height={this.fdHeight}/>,
+            <video autoplay playsinline width="1" height="1"/>,
             <canvas width={this.fdWidth} height={this.fdHeight} />
         ]
     }
@@ -73,22 +75,98 @@ export class WafFaceDetect {
     }
 
     private initCanvas() {
-        const canvasContext:CanvasRenderingContext2D = this.fdCanvas.getContext('2d');
+        // memory allocations
+        const allocations = this.faceDetectorAllocateMemory(this.fdWidth, this.fdHeight);
 
         // looping draw to canvas function
-        const drawToCanvaLoopBuilder = function(ctx, video) {
-            return function() {
-                ctx.drawImage(video, 0, 0);
-                window.requestAnimationFrame(drawToCanvaLoopBuilder(ctx, video));
-            }
+        const drawToCanvaLoop = () => {
+            const ctx:CanvasRenderingContext2D = this.fdCanvas.getContext('2d');
+            const video:HTMLVideoElement = this.fdVideo;
+            const width:number = this.fdWidth;
+            const height:number = this.fdHeight;
+
+            // draw on canvas
+            ctx.drawImage(video, 0, 0);
+
+            // retrieve data and pass it for face detection
+            const rgbaData:ImageData = ctx.getImageData(0, 0, width, height);
+            const dets = this.faceDetectionCalculate(rgbaData, width, height, allocations);
+            this.wafFaceDetectorEE.emit(dets);
+
+            // setup for next repaint
+            window.requestAnimationFrame(drawToCanvaLoop);
         }
 
-        console.log(this.wasmFaceDetectorModule);
-
         // update at each browser refresh (and pause when tab is not in focus)
-        window.requestAnimationFrame(drawToCanvaLoopBuilder(canvasContext, this.fdVideo));
+        window.requestAnimationFrame(drawToCanvaLoop);
 
         // canvas setup is finished resolve promise (not really necessary but keep initSubSteps with the same interface)
         return Promise.resolve();
+    }
+
+    private faceDetectorAllocateMemory(width:number, height:number, maxndetections:number = 1024) {
+        // allocate memory inside wasm module
+        let ppixels = this.wasmFaceDetectorModule._malloc(width*height);
+        let pixels = new Uint8Array(this.wasmFaceDetectorModule.HEAPU8.buffer, ppixels, width*height);
+
+        // allocate memory for detection
+        let prcsq = this.wasmFaceDetectorModule._malloc(4*4*maxndetections);
+        let rscq = new Float32Array(this.wasmFaceDetectorModule.HEAPU8.buffer, prcsq, maxndetections);
+
+        return {
+            ppixels: ppixels,
+            pixels: pixels,
+            prcsq: prcsq,
+            rscq: rscq,
+            maxndetections: maxndetections
+        }
+    }
+
+    private faceDetectionCalculate(image:ImageData, width:number, height:number, allocations:any) {
+        const rgba = image.data;
+        const ppixels = allocations.ppixels;
+        const pixels = allocations.pixels;
+        const prcsq = allocations.prcsq;
+        const rcsq = allocations.rscq;
+        const maxndetections = allocations.maxndetections;
+        const params = {
+            shiftfactor: 0.1, // move the detection window by 10% of its size
+            minsize: 100,     // minimum size of a face
+            maxsize: 1000,    // maximum size of a face
+            scalefactor: 1.1  // for multiscale processing: resize the detection window by 10% when moving to the higher scale
+        }
+
+        // convert pixels to gray scale & move the RGBA pixels to the Wasm memory
+        Array(height).fill('fake').forEach((_itemh, r) => {
+            Array(width).fill('fake').forEach((_itemw, c) => {
+                // gray = 0.2*red + 0.7*green + 0.1*blue
+                pixels[r*width + c] = (2*rgba[r*4*width+4*c+0]+7*rgba[r*4*width+4*c+1]+1*rgba[r*4*width+4*c+2])/10;
+            })
+        })
+
+        // run the detector across the frame
+        // rcsq is an array representing row, column, scale and detection score
+        let ndetections = this.wasmFaceDetectorModule._find_faces(prcsq, maxndetections, ppixels, height, width, width, params.scalefactor, params.shiftfactor, params.minsize, params.maxsize);
+        
+        // cluster multiple detections found around each face
+        ndetections = this.wasmFaceDetectorModule._cluster_detections(prcsq, ndetections);
+        
+        let dets = [];
+        const ctx:CanvasRenderingContext2D = this.fdCanvas.getContext('2d');
+        Array(ndetections).fill('fake').forEach((_item, i) => {
+            // return detections positions
+            if (rcsq[4*i+3] > this.detectionThreshold) {
+                dets.push({ x: rcsq[4*i+1], y: rcsq[4*i+0], size: rcsq[4*i+2] });
+
+                // draw detection
+                ctx.beginPath();
+                ctx.arc(rcsq[4*i+1], rcsq[4*i+0], rcsq[4*i+2]/2, 0, 2*Math.PI, false);
+                ctx.lineWidth = 2;
+                ctx.strokeStyle = 'red';
+                ctx.stroke();
+            }
+        });
+
+        return dets;
     }
 }
