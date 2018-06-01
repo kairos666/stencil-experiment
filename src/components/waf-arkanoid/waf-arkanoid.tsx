7fd8428c22d6f7ff35f28de89051a7aa883466c2
David Maggi <david.maggi@orange.com>
2018-06-01 23:10:33 +0200

    [waf-arkanoid] collision detection

--- src/components/waf-arkanoid/waf-arkanoid.tsx
+++ src/components/waf-arkanoid/waf-arkanoid.tsx
@@ -27,7 +27,8 @@ export class WafArkanoid {
             bottomMargin: 20
         },
         ball: {
-            initialSpeed: 2,
+            initialSpeed: 0.1,
+            acceleration: 0.00001,
             ballRadius: 10,
             ballColor: '#0093e0'
         },
@@ -73,7 +74,7 @@ export class WafArkanoid {
             const dt:number = this.elapsedTime(DHRTimeStamp);
             
             // collision detection --> movement prediction
-            this.update(this.model, dt);
+            this.model = this.update(this.model, dt);
 
             // draw game state
             this.drawModel(this.model);
@@ -83,16 +84,74 @@ export class WafArkanoid {
     }
 
     private update(model, dt) {
+        // future position
+        let pos = this.accelerate(model.ball.x, model.ball.y, model.ball.dx, model.ball.dy, model.ball.accel ,dt);
+        let pt;
         // collision detection - side walls
-        this.sideCollision(model, dt);
+        pos = this.sideCollision(pos, model, this.width, this.height);
         // collision detection - bricks
+        pt = this.bricksCollision();
         // collision detection - paddle
-        // collision detection - pit (game over)
+        pt = this.paddleCollision(pos, model);
+
+        // collision occured - react
+        if(pt) {
+            switch(pt.d) {
+                case 'left':
+                case 'right':
+                    pos.x = pt.x;
+                    pos.dx = -pos.dx;
+                    break;
+                case 'top':
+                case 'bottom':
+                    pos.y = pt.y;
+                    pos.dy = -pos.dy;
+                    break;
+            }
+        }
+
+        // merge pos into model
+        model.ball = Object.assign(model.ball, pos);
+        
+        return model;
     }
 
-    private sideCollision(model, dt) {
-        model;
-        dt;
+    private sideCollision(initialPos, model, width, height) {
+        const pos = Object.assign({}, initialPos);
+        const radius = model.ball.radius;
+        const bottomThreshold = height;
+        const topThreshold = WafArkanoid.config.bricks.sideSpace + radius;
+        const leftThreshold = WafArkanoid.config.bricks.sideSpace + radius;
+        const rightThreshold = width - WafArkanoid.config.bricks.sideSpace - radius;
+
+        // top 
+        if ((pos.dy < 0) && (pos.y < topThreshold)) {
+            pos.dy = -pos.dy; 
+            pos.y = topThreshold;
+        }
+        // bottom (game over) 
+        if ((pos.dy > 0) && (pos.y > bottomThreshold)) {
+            pos.dy = -pos.dy;
+            pos.y = bottomThreshold;
+        }
+        // left 
+        if ((pos.dx < 0) && (pos.x < leftThreshold)) {
+            pos.dx = -pos.dx; 
+            pos.x = leftThreshold;
+        }
+        // right 
+        if ((pos.dx) > 0 && (pos.x > rightThreshold)) {
+            pos.dx = -pos.dx; 
+            pos.x = rightThreshold;
+        }
+
+        return pos;
+    }
+
+    private bricksCollision() {}
+
+    private paddleCollision(pos, model) {
+        return this.ballIntercept(model.ball, model.paddle, pos.nx, pos.ny);
     }
 
     private drawModel(model) {
@@ -182,8 +241,76 @@ export class WafArkanoid {
             y: paddleModel.y - config.ballRadius, 
             dx: config.initialSpeed, 
             dy: -config.initialSpeed, 
+            accel: config.acceleration,
             radius: config.ballRadius, 
             color: config.ballColor
         };
     }
+
+    private accelerate(x, y, dx, dy, accel, dt) {
+        let x2  = x + (dt * dx) + (accel * dt * dt * 0.5);
+        let y2  = y + (dt * dy) + (accel * dt * dt * 0.5);
+        let dx2 = dx + (accel * dt) * (dx > 0 ? 1 : -1);
+        let dy2 = dy + (accel * dt) * (dy > 0 ? 1 : -1);
+        return { nx: (x2-x), ny: (y2-y), x: x2, y: y2, dx: dx2, dy: dy2 };
+    }
+
+    private intercept(x1, y1, x2, y2, x3, y3, x4, y4, d) {
+        let denom = ((y4-y3) * (x2-x1)) - ((x4-x3) * (y2-y1));
+        if (denom != 0) {
+            let ua = (((x4-x3) * (y1-y3)) - ((y4-y3) * (x1-x3))) / denom;
+            if ((ua >= 0) && (ua <= 1)) {
+                let ub = (((x2-x1) * (y1-y3)) - ((y2-y1) * (x1-x3))) / denom;
+                if ((ub >= 0) && (ub <= 1)) {
+                    let x = x1 + (ua * (x2-x1));
+                    let y = y1 + (ua * (y2-y1));
+                    return { x: x, y: y, d: d};
+                }
+            }
+        }
+        return null;
+    }
+
+    private ballIntercept(ball, rect, nx, ny) {
+        const top = rect.y - ball.radius;
+        const bottom = rect.y + rect.height + ball.radius;
+        const left = rect.x - ball.radius;
+        const right = rect.x + rect.width + ball.radius;
+        let pt;
+        if (nx < 0) {
+          pt = this.intercept(ball.x, ball.y, ball.x + nx, ball.y + ny, 
+            right,
+            top,
+            right,
+            bottom,
+            "right");
+        }
+        else if (nx > 0) {
+          pt = this.intercept(ball.x, ball.y, ball.x + nx, ball.y + ny,
+            left,
+            top,
+            left,
+            bottom,
+            "left");
+        }
+        if (!pt) {
+          if (ny < 0) {
+            pt = this.intercept(ball.x, ball.y, ball.x + nx, ball.y + ny,
+            left,
+            bottom,
+            right,
+            bottom,
+            "bottom");
+          }
+          else if (ny > 0) {
+            pt = this.intercept(ball.x, ball.y, ball.x + nx, ball.y + ny,
+            left,
+            top,
+            right,
+            top,
+            "top");
+          }
+        }
+        return pt;
+      }
 }
