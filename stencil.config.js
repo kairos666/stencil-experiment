0ab03bd2c83c104a305f2704c32d65de0d571286
David Maggi <david.maggi@orange.com>
2018-07-15 09:44:47 +0200

    [pre-merge] conflicted files

--- stencil.config.js
+++ stencil.config.js
@@ -1,12 +1,24 @@
+const sass = require('@stencil/sass');
+
 exports.config = {
-  namespace: 'mycomponent',
-  outputTargets:[
-    {
-      type: 'dist'
-    },
-    {
-      type: 'www',
-      serviceWorker: false
-    }
-  ]
+    namespace: 'waf',
+    plugins: [
+        sass()
+    ],
+    outputTargets: [{
+            type: 'dist'
+        },
+        {
+            type: 'www',
+            serviceWorker: false
+        }
+    ],
+    copy: [
+        { src: './wcs-assets/**/*' }
+    ]
 };
+
+exports.devServer = {
+    root: 'www',
+    watchGlob: '**/**'
+}
